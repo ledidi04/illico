434838043f54b0800365128dc98b89c2bece428b
ledidi04 <enilsonetienne04@gmail.com>
2026-04-24 13:45:43 -0700

    modified:   index.php

--- index.php
+++ index.php
@@ -40,6 +40,7 @@ $date_taux = date('d/m/Y');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="ikYgHEBCKlQWimzFOnFPfwoOUqXJ41fPNc6nXbI1lng" />
     <title>S&P illico — Banque Communautaire</title>
     <link rel="icon" type="image/jpeg" href="main/logo.jpeg">
     <base href="<?= $base_url ?>">
